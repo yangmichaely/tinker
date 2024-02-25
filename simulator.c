b86a6153c59d7b1948377ba41347491a05d33c13
yangmichaely <yangmichaely@gmail.com>
2024-02-25 01:26:29 -0600

    baiughiuaeg

--- simulator.c
+++ simulator.c
@@ -7,7 +7,7 @@
 #include <limits.h>
 #include <regex.h>
 
-CPU cpu;
+//CPU cpu;
 int nextMem;
 
 int main(int argc, char** argv){
@@ -39,120 +39,120 @@ int memCheck(uint64_t index){
     return 0;
 }
 
-int add(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].sinteger64 = cpu.regs[rs].sinteger64 + cpu.regs[rt].sinteger64;
-    cpu.pc += 4;
+int add(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].sinteger64 = cpu -> regs[rs].sinteger64 + cpu -> regs[rt].sinteger64;
+    cpu -> pc += 4;
 }
 
-void addi(uint64_t rd, uint64_t l){
-    cpu.regs[rd].uinteger64 += l;
-    cpu.pc += 4;
+void addi(uint64_t rd, uint64_t l, CPU* cpu){
+    cpu -> regs[rd].uinteger64 += l;
+    cpu -> pc += 4;
 }
 
-void sub(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].sinteger64 = cpu.regs[rs].sinteger64 - cpu.regs[rt].sinteger64;
-    cpu.pc += 4;
+void sub(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].sinteger64 = cpu -> regs[rs].sinteger64 - cpu -> regs[rt].sinteger64;
+    cpu -> pc += 4;
 }
 
-void subi(uint64_t rd, uint64_t l){
-    cpu.regs[rd].sinteger64 -= l;
-    cpu.pc += 4;
+void subi(uint64_t rd, uint64_t l, CPU* cpu){
+    cpu -> regs[rd].sinteger64 -= l;
+    cpu -> pc += 4;
 }
 
-void mul(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].sinteger64 = cpu.regs[rs].sinteger64 * cpu.regs[rt].sinteger64;
-    cpu.pc += 4;
+void mul(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].sinteger64 = cpu -> regs[rs].sinteger64 * cpu -> regs[rt].sinteger64;
+    cpu -> pc += 4;
 }
 
-void divide(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].sinteger64 = cpu.regs[rs].sinteger64 / cpu.regs[rt].sinteger64;
-    cpu.pc += 4;
+void divide(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].sinteger64 = cpu -> regs[rs].sinteger64 / cpu -> regs[rt].sinteger64;
+    cpu -> pc += 4;
 }
 
-void and(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].uinteger64 = cpu.regs[rs].uinteger64 & cpu.regs[rt].uinteger64;
-    cpu.pc += 4;
+void and(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].uinteger64 = cpu -> regs[rs].uinteger64 & cpu -> regs[rt].uinteger64;
+    cpu -> pc += 4;
 }
 
-void or(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].uinteger64 = cpu.regs[rs].uinteger64 | cpu.regs[rt].uinteger64;
-    cpu.pc += 4;
+void or(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].uinteger64 = cpu -> regs[rs].uinteger64 | cpu -> regs[rt].uinteger64;
+    cpu -> pc += 4;
 }
 
-void xor(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].uinteger64 = cpu.regs[rs].uinteger64 ^ cpu.regs[rt].uinteger64;
-    cpu.pc += 4;
+void xor(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].uinteger64 = cpu -> regs[rs].uinteger64 ^ cpu -> regs[rt].uinteger64;
+    cpu -> pc += 4;
 }
 
-void not(uint64_t rd, uint64_t rs){
-    cpu.regs[rd].uinteger64 = ~cpu.regs[rs].uinteger64;
-    cpu.pc += 4;
+void not(uint64_t rd, uint64_t rs, CPU* cpu){
+    cpu -> regs[rd].uinteger64 = ~cpu -> regs[rs].uinteger64;
+    cpu -> pc += 4;
 }
 
-void shftr(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].uinteger64 = cpu.regs[rs].uinteger64 >> cpu.regs[rt].uinteger64;
-    cpu.pc += 4;
+void shftr(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].uinteger64 = cpu -> regs[rs].uinteger64 >> cpu -> regs[rt].uinteger64;
+    cpu -> pc += 4;
 }
 
-void shftri(uint64_t rd, uint64_t l){
-    cpu.regs[rd].uinteger64 = cpu.regs[rd].uinteger64 >> l;
-    cpu.pc += 4;
+void shftri(uint64_t rd, uint64_t l, CPU* cpu){
+    cpu -> regs[rd].uinteger64 = cpu -> regs[rd].uinteger64 >> l;
+    cpu -> pc += 4;
 }
 
-void shftl(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].uinteger64 = cpu.regs[rs].uinteger64 << cpu.regs[rt].uinteger64;
-    cpu.pc += 4;
+void shftl(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].uinteger64 = cpu -> regs[rs].uinteger64 << cpu -> regs[rt].uinteger64;
+    cpu -> pc += 4;
 }
 
-void shftli(uint64_t rd, uint64_t l){
-    cpu.regs[rd].uinteger64 = cpu.regs[rd].uinteger64 << l;
-    cpu.pc += 4;
+void shftli(uint64_t rd, uint64_t l, CPU* cpu){
+    cpu -> regs[rd].uinteger64 = cpu -> regs[rd].uinteger64 << l;
+    cpu -> pc += 4;
 }
 
-void br(uint64_t rd){
-    cpu.pc = cpu.regs[rd].uinteger64;
+void br(uint64_t rd, CPU* cpu){
+    cpu -> pc = cpu -> regs[rd].uinteger64;
 }
 
-void brrReg(uint64_t rd){
-    cpu.pc += cpu.regs[rd].uinteger64;
+void brrReg(uint64_t rd, CPU* cpu){
+    cpu -> pc += cpu -> regs[rd].uinteger64;
 }
 
-void brrL(uint64_t l){
-    cpu.pc += l;
+void brrL(uint64_t l, CPU* cpu){
+    cpu -> pc += l;
 }
 
-void brnz(uint64_t rd, uint64_t rs){
-    if(cpu.regs[rs].uinteger64 == 0){
-        cpu.pc += 4;
+void brnz(uint64_t rd, uint64_t rs, CPU* cpu){
+    if(cpu -> regs[rs].uinteger64 == 0){
+        cpu -> pc += 4;
     }
     else{
-        cpu.pc = cpu.regs[rd].uinteger64;
+        cpu -> pc = cpu -> regs[rd].uinteger64;
     }
 }
 
-int call(uint64_t rd, uint64_t rs, uint64_t rt){
+int call(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
     //for(int i = 0; i < 4; i++){
-        if(memCheck(cpu.regs[31].uinteger64 - 8) != 0){
+        if(memCheck(cpu -> regs[31].uinteger64 - 8) != 0){
             return -1;
         }
     //}
-    // uint32_t value = (uint32_t) cpu.pc + 4;
+    // uint32_t value = (uint32_t) cpu -> pc + 4;
     // cpu.mem[cpu.regs[31].uinteger64 - 8] = (0xff000000 & value) >> 24;
     // cpu.mem[cpu.regs[31].uinteger64 - 7] = (0x00ff0000 & value) >> 16;
     // cpu.mem[cpu.regs[31].uinteger64 - 6] = (0x0000ff00 & value) >> 8;
     // cpu.mem[cpu.regs[31].uinteger64 - 5] = 0xff & value;
     //uint32_t value = (uint32_t) cpu.pc + 4;
-    cpu.mem[cpu.regs[31].uinteger64 - 8] = cpu.pc + 4;
+    cpu -> mem[cpu -> regs[31].uinteger64 - 8] = cpu -> pc + 4;
     // for(int i = 0; i < 4; i++){
     //     cpu.mem[cpu.regs[31].uinteger64 - 8 + i] = (((long)0xff << i * 8) & value) >> i * 8;
     // }
-    cpu.pc = cpu.regs[rd].uinteger64;
+    cpu -> pc = cpu -> regs[rd].uinteger64;
     return 0;
 }
 
-int ret(){
+int ret(CPU* cpu){
     //for(int i = 0; i < 8; i++){
-        if(memCheck(cpu.regs[31].uinteger64 - 8) != 0){
+        if(memCheck(cpu -> regs[31].uinteger64 - 8) != 0){
             return -1;
         }
     //}
@@ -169,28 +169,28 @@ int ret(){
     // val |= cpu.mem[cpu.regs[31].uinteger64 - 6] << 8;
     // val |= cpu.mem[cpu.regs[31].uinteger64 - 5];
     // cpu.pc = val;
-    cpu.pc = cpu.mem[cpu.regs[31].uinteger64 - 8];
+    cpu -> pc = cpu -> mem[cpu -> regs[31].uinteger64 - 8];
     return 0;
 }
 
-void brgt(uint64_t rd, uint64_t rs, uint64_t rt){
-    if(cpu.regs[rs].sinteger64 <= cpu.regs[rt].sinteger64){
-        cpu.pc += 4;
+void brgt(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    if(cpu -> regs[rs].sinteger64 <= cpu -> regs[rt].sinteger64){
+        cpu -> pc += 4;
     }
     else{
-        cpu.pc = cpu.regs[rd].uinteger64;
+        cpu -> pc = cpu -> regs[rd].uinteger64;
     }
 }
 
-int movReadMemStoreReg(uint64_t rd, uint64_t rs, uint64_t l){
+int movReadMemStoreReg(uint64_t rd, uint64_t rs, uint64_t l, CPU* cpu){
     for(int i = 0; i < 8; i++){
-        if(memCheck(cpu.regs[rd].uinteger64 + l + i) != 0){
+        if(memCheck(cpu -> regs[rd].uinteger64 + l + i) != 0){
             return -1;
         }
     }
     uint64_t val = 0;
     for(int i = 0; i < 8; i++){
-        val |= (uint64_t) cpu.mem[cpu.regs[rs].uinteger64 + l + i] << (i * 8);
+        val |= (uint64_t) cpu -> mem[cpu -> regs[rs].uinteger64 + l + i] << (i * 8);
     }
     // val |= (uint64_t) cpu.mem[cpu.regs[rs].uinteger64 + l + 4] << 56;
     // val |= (uint64_t) cpu.mem[cpu.regs[rs].uinteger64 + l + 5] << 48;
@@ -200,31 +200,31 @@ int movReadMemStoreReg(uint64_t rd, uint64_t rs, uint64_t l){
     // val |= cpu.mem[cpu.regs[rs].uinteger64 + l + 1] << 16;
     // val |= cpu.mem[cpu.regs[rs].uinteger64 + l + 2] << 8;
     // val |= cpu.mem[cpu.regs[rs].uinteger64 + l + 3];
-    cpu.regs[rd].uinteger64 = val;
-    cpu.pc += 4;
+    cpu -> regs[rd].uinteger64 = val;
+    cpu -> pc += 4;
     return 0;
 }
 
-void movReadRegStoreReg(uint64_t rd, uint64_t rs){
-    cpu.regs[rd].uinteger64 = cpu.regs[rs].uinteger64;
-    cpu.pc += 4;
+void movReadRegStoreReg(uint64_t rd, uint64_t rs, CPU* cpu){
+    cpu -> regs[rd].uinteger64 = cpu -> regs[rs].uinteger64;
+    cpu -> pc += 4;
 }
 
-void movSetBits(uint64_t rd, uint64_t l){
-    cpu.regs[rd].uinteger64 &= 0xfffffffffffff000;
-    cpu.regs[rd].uinteger64 |= l;
-    cpu.pc += 4;
+void movSetBits(uint64_t rd, uint64_t l, CPU* cpu){
+    cpu -> regs[rd].uinteger64 &= 0xfffffffffffff000;
+    cpu -> regs[rd].uinteger64 |= l;
+    cpu -> pc += 4;
 }
 
-int movReadRegStoreMem(uint64_t rd, uint64_t rs, uint64_t l){
+int movReadRegStoreMem(uint64_t rd, uint64_t rs, uint64_t l, CPU* cpu){
     for(int i = 0; i < 8; i++){
-        if(memCheck(cpu.regs[rd].uinteger64 + l + i) != 0){
+        if(memCheck(cpu -> regs[rd].uinteger64 + l + i) != 0){
             return -1;
         }
     }
-    uint64_t value = cpu.regs[rs].uinteger64;
+    uint64_t value = cpu -> regs[rs].uinteger64;
     for(int i = 0; i < 8; i++){
-        cpu.mem[cpu.regs[rd].uinteger64 + l + i] = (((long)0xff << i * 8) & value) >> i * 8;
+        cpu -> mem[cpu -> regs[rd].uinteger64 + l + i] = (((long)0xff << i * 8) & value) >> i * 8;
     }
     // cpu.mem[cpu.regs[rd].uinteger64 + l] = (0x00000000ff000000 & value) >> 24;
     // cpu.mem[cpu.regs[rd].uinteger64 + l + 1] = (0x0000000000ff0000 & value) >> 16;
@@ -234,161 +234,161 @@ int movReadRegStoreMem(uint64_t rd, uint64_t rs, uint64_t l){
     // cpu.mem[cpu.regs[rd].uinteger64 + l + 5] = (0x00ff000000000000 & value) >> 48;
     // cpu.mem[cpu.regs[rd].uinteger64 + l + 6] = (0x0000ff0000000000 & value) >> 40;
     // cpu.mem[cpu.regs[rd].uinteger64 + l + 7] = 0x000000ff00000000 & value >> 32;
-    cpu.pc += 4;
+    cpu -> pc += 4;
     return 0;
 }
 
-void addf(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].floatingPoint = cpu.regs[rs].floatingPoint + cpu.regs[rt].floatingPoint;
-    cpu.pc += 4;
+void addf(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].floatingPoint = cpu -> regs[rs].floatingPoint + cpu -> regs[rt].floatingPoint;
+    cpu -> pc += 4;
 }
 
-void subf(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].floatingPoint = cpu.regs[rs].floatingPoint - cpu.regs[rt].floatingPoint;
-    cpu.pc += 4;
+void subf(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].floatingPoint = cpu -> regs[rs].floatingPoint - cpu -> regs[rt].floatingPoint;
+    cpu -> pc += 4;
 }
 
-void mulf(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].floatingPoint = cpu.regs[rs].floatingPoint * cpu.regs[rt].floatingPoint;
-    cpu.pc += 4;
+void mulf(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].floatingPoint = cpu -> regs[rs].floatingPoint * cpu -> regs[rt].floatingPoint;
+    cpu -> pc += 4;
 }
 
-void divf(uint64_t rd, uint64_t rs, uint64_t rt){
-    cpu.regs[rd].floatingPoint = cpu.regs[rs].floatingPoint / cpu.regs[rt].floatingPoint;
-    cpu.pc += 4;
+void divf(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu){
+    cpu -> regs[rd].floatingPoint = cpu -> regs[rs].floatingPoint / cpu -> regs[rt].floatingPoint;
+    cpu -> pc += 4;
 }
 
-int in(uint64_t rd, uint64_t rs){
-    if(cpu.regs[rs].uinteger64 == 0){
-        scanf("%lu", &cpu.regs[rd].uinteger64);
-        cpu.pc += 4;
+int in(uint64_t rd, uint64_t rs, CPU* cpu){
+    if(cpu -> regs[rs].uinteger64 == 0){
+        scanf("%lu", &cpu -> regs[rd].uinteger64);
+        cpu -> pc += 4;
         return 0;
     }
 }
 
-int out(uint64_t rd, uint64_t rs){
-    if(cpu.regs[rd].uinteger64 == 1){
-        printf("%lu\n", cpu.regs[rs].uinteger64);
-        cpu.pc += 4;
+int out(uint64_t rd, uint64_t rs, CPU* cpu){
+    if(cpu -> regs[rd].uinteger64 == 1){
+        printf("%lu\n", cpu -> regs[rs].uinteger64);
+        cpu -> pc += 4;
         return 0;
     }
 }
 
-int interpret(uint64_t opcode, uint64_t rd, uint64_t rs, uint64_t rt, uint64_t l, uint64_t* pc){
+int interpret(uint64_t opcode, uint64_t rd, uint64_t rs, uint64_t rt, uint64_t l, CPU* cpu){
     switch (opcode){
         case 0:
-            add(rd, rs, rt, *pc);
+            add(rd, rs, rt, &cpu);
             return 0;
         case 1:
-            addi(rd, l);
+            addi(rd, l, &cpu);
             return 0;
         case 2:
-            sub(rd, rs, rt);
+            sub(rd, rs, rt, &cpu);
             return 0;
         case 3:
-            subi(rd, l);
+            subi(rd, l, &cpu);
             return 0;
         case 4:
-            mul(rd, rs, rt);
+            mul(rd, rs, rt, &cpu);
             return 0;
         case 5:
             if(rt == 0){
                 return -1;
             }
-            divide(rd, rs, rt);
+            divide(rd, rs, rt, &cpu);
             return 0;
         case 6:
-            and(rd, rs, rt);
+            and(rd, rs, rt, &cpu);
             return 0;
         case 7:
-            or(rd, rs, rt);
+            or(rd, rs, rt, &cpu);
             return 0;
         case 8:
-            xor(rd, rs, rt);
+            xor(rd, rs, rt, &cpu);
             return 0;
         case 9:
-            not(rd, rs);
+            not(rd, rs, &cpu);
             return 0;
         case 10:
-            shftr(rd, rs, rt);
+            shftr(rd, rs, rt, &cpu);
             return 0;
         case 11:
-            shftri(rd, l);
+            shftri(rd, l, &cpu);
             return 0;
         case 12:
-            shftl(rd, rs, rt);
+            shftl(rd, rs, rt, &cpu);
             return 0;
         case 13:
-            shftli(rd, l);
+            shftli(rd, l, &cpu);
             return 0;
         case 14:
-            br(rd);
+            br(rd, &cpu);
             return 0;
         case 15:
-            brrReg(rd);
+            brrReg(rd, &cpu);
             return 0;
         case 16:
-            brrReg(l);
+            brrReg(l, &cpu);
             return 0;
         case 17:
-            brnz(rd, rs);
+            brnz(rd, rs, &cpu);
             return 0;
         case 18:
-            if(call(rd, rs, rt) == 0){
+            if(call(rd, rs, rt, &cpu) == 0){
                 return 0;
             }
             return -1;
         case 19:
-            if(ret() == 0){
+            if(ret(&cpu) == 0){
                 return 0;
             }
             return -1;
         case 20:
-            brgt(rd, rs, rt);
+            brgt(rd, rs, rt, &cpu);
             return 0;
         case 21:
-            if(movReadMemStoreReg(rd, rs, l) == 0){
+            if(movReadMemStoreReg(rd, rs, l, &cpu) == 0){
                 return 0;
             }
             return -1;
         case 22:
-            movReadRegStoreReg(rd, rs);
+            movReadRegStoreReg(rd, rs, &cpu);
             return 0;
         case 23:
-            movSetBits(rd, l);
+            movSetBits(rd, l, &cpu);
             return 0;
         case 24:
-            if(movReadRegStoreMem(rd, rs, l) == 0){
+            if(movReadRegStoreMem(rd, rs, l, &cpu) == 0){
                 return 0;
             }
             return -1;
         case 25:
-            addf(rd, rs, rt);
+            addf(rd, rs, rt, &cpu);
             return 0;
         case 26:
-            subf(rd, rs, rt);
+            subf(rd, rs, rt, &cpu);
             return 0;
         case 27:
-            mulf(rd, rs, rt);
+            mulf(rd, rs, rt, &cpu);
             return 0;
         case 28:
             if(rt == 0){
                 return -1;
             }
-            divf(rd, rs, rt);
+            divf(rd, rs, rt, &cpu);
             return 0;
         case 29:
-            if(in(rd, rs) == 0){
+            if(in(rd, rs, &cpu) == 0){
                 return 0;
             }
             return -1;
         case 30:
-            if(out(rd, rs) == 0){
+            if(out(rd, rs, &cpu) == 0){
                 return 0;
             }
             return -1;
         case 31:
-            cpu.pc += 4;
+            cpu -> pc += 4;
             return 1;
         default:
             return -1;
@@ -397,6 +397,7 @@ int interpret(uint64_t opcode, uint64_t rd, uint64_t rs, uint64_t rt, uint64_t l
 }
 
 int readBinary(FILE* f){
+    CPU cpu;
     for(int i = 0; i < MEM_SIZE; i++){
         cpu.mem[i] = 0;
     }
@@ -443,7 +444,7 @@ int readBinary(FILE* f){
         // printf("rs: %ld\n", rs);
         // printf("rt: %ld\n", rt);
         // printf("l: %ld\n", l);
-        check = interpret(opcode, rd, rs, rt, l, &cpu.pc);
+        check = interpret(opcode, rd, rs, rt, l, &cpu);
         if(check == -1){
             fprintf(stderr, "%s\n", "Simulation error");
             exit(-1);
--- simulator.h
+++ simulator.h
@@ -24,69 +24,69 @@ typedef struct CPU {
 
 int memCheck(uint64_t index);
 
-int add(uint64_t rd, uint64_t rs, uint64_t rt);
+int add(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-void addi(uint64_t rd, uint64_t l);
+void addi(uint64_t rd, uint64_t l, CPU* cpu);
 
-void sub(uint64_t rd, uint64_t rs, uint64_t rt);
+void sub(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-void subi(uint64_t rd, uint64_t l);
+void subi(uint64_t rd, uint64_t l, CPU* cpu);
 
-void mul(uint64_t rd, uint64_t rs, uint64_t rt);
+void mul(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-void divide(uint64_t rd, uint64_t rs, uint64_t rt);
+void divide(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-void and(uint64_t rd, uint64_t rs, uint64_t rt);
+void and(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-void or(uint64_t rd, uint64_t rs, uint64_t rt);
+void or(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-void xor(uint64_t rd, uint64_t rs, uint64_t rt);
+void xor(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-void not(uint64_t rd, uint64_t rs);
+void not(uint64_t rd, uint64_t rs, CPU* cpu);
 
-void shftr(uint64_t rd, uint64_t rs, uint64_t rt);
+void shftr(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-void shftri(uint64_t rd, uint64_t l);
+void shftri(uint64_t rd, uint64_t l, CPU* cpu);
 
-void shftl(uint64_t rd, uint64_t rs, uint64_t rt);
+void shftl(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-void shftli(uint64_t rd, uint64_t l);
+void shftli(uint64_t rd, uint64_t l, CPU* cpu);
 
-void br(uint64_t rd);
+void br(uint64_t rd, CPU* cpu);
 
-void brrReg(uint64_t rd);
+void brrReg(uint64_t rd, CPU* cpu);
 
-void brrL(uint64_t l);
+void brrL(uint64_t l, CPU* cpu);
 
-void brnz(uint64_t rd, uint64_t rs);
+void brnz(uint64_t rd, uint64_t rs, CPU* cpu);
 
-int call(uint64_t rd, uint64_t rs, uint64_t rt);
+int call(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-int ret();
+int ret(CPU* cpu);
 
-void brgt(uint64_t rd, uint64_t rs, uint64_t rt);
+void brgt(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-int movReadMemStoreReg(uint64_t rd, uint64_t rs, uint64_t l);
+int movReadMemStoreReg(uint64_t rd, uint64_t rs, uint64_t l, CPU* cpu);
 
-void movReadRegStoreReg(uint64_t rd, uint64_t rs);
+void movReadRegStoreReg(uint64_t rd, uint64_t rs, CPU* cpu);
 
-void movSetBits(uint64_t rd, uint64_t l);
+void movSetBits(uint64_t rd, uint64_t l, CPU* cpu);
 
-int movReadRegStoreMem(uint64_t rd, uint64_t rs, uint64_t l);
+int movReadRegStoreMem(uint64_t rd, uint64_t rs, uint64_t l, CPU* cpu);
 
-void addf(uint64_t rd, uint64_t rs, uint64_t rt);
+void addf(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-void subf(uint64_t rd, uint64_t rs, uint64_t rt);
+void subf(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-void mulf(uint64_t rd, uint64_t rs, uint64_t rt);
+void mulf(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-void divf(uint64_t rd, uint64_t rs, uint64_t rt);
+void divf(uint64_t rd, uint64_t rs, uint64_t rt, CPU* cpu);
 
-int in(uint64_t rd, uint64_t rs);
+int in(uint64_t rd, uint64_t rs, CPU* cpu);
 
-int out(uint64_t rd, uint64_t rs);
+int out(uint64_t rd, uint64_t rs, CPU* cpu);
 
-int interpret(uint64_t opcode, uint64_t rd, uint64_t rs, uint64_t rt, uint64_t l, uint64_t* pc);
+int interpret(uint64_t opcode, uint64_t rd, uint64_t rs, uint64_t rt, uint64_t l, CPU* cpu);
 
 int readBinary(FILE* f);
 
