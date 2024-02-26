5280271cdda9b19192d2998c3456a4cb3e4d8d55
yangmichaely <yangmichaely@gmail.com>
2024-02-26 01:04:37 -0600

    h

--- simulator.c
+++ simulator.c
@@ -127,7 +127,7 @@ int interpret(uint64_t opcode, uint64_t rd, uint64_t rs, uint64_t rt, uint64_t l
             if(memCheck(cpu.regs[31].uinteger64 - 8) != 0){
                 return -1;
             }
-            cpu.pc = cpu.mem[cpu.regs[31].uinteger64 - 8];
+            cpu.pc = *(uint64_t*)(&cpu.mem[cpu.regs[31].uinteger64 - 8]);
             return 0;
         case 20:
             if(cpu.regs[rs].sinteger64 <= cpu.regs[rt].sinteger64){
@@ -192,7 +192,7 @@ int interpret(uint64_t opcode, uint64_t rd, uint64_t rs, uint64_t rt, uint64_t l
             return 0;
         case 29:
             if(cpu.regs[rs].uinteger64 == 0){
-                scanf("%llu\n", &cpu.regs[rd].uinteger64);
+                scanf("%lu\n", &cpu.regs[rd].uinteger64);
                 cpu.pc += 4;
             }
             return 0;
